4e1abe3d036acc6e954ec558a59234ed324f18a1
Brian Chepchieng <chepchiengbrian@gmail.com>
2023-03-16 00:13:16 -0700

    Do square of integer matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,4 @@
+#!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-
-    rows = len(matrix)
-    cols = len(matrix[0])
-
-    squared_matrix = [[0 for j in range(cols)] for i in range(rows)]
-    for i in range(rows):
-        for j in range(cols):
-            squared_matrix[i][j] = matrix[i][j] ** 2
-
-    for row in squared_matrix:
-        return squared_matrix
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
